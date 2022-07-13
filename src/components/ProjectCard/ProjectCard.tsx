565f7f16da606e0265dc37730ea666ef75218e1f
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-07-13 13:14:58 +0200

    add projectsList with card component

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import '../../styles/components/_projectCard.scss';
+import Button from '../Button/Button';
+
+const ProjectCard = ({ project }: any) => {
+  console.log(project);
+  return (
+    <div className='card'>
+      <div className='card-image'></div>
+      <h4>{project.name}</h4>
+      <p>{project.description}</p>
+      <Link className='card-link' to='/'>
+        <Button type='button' colour='btn--primary' text='View' />
+      </Link>
+    </div>
+  );
+};
+
+export default ProjectCard;
---
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from '@apollo/client';
+import { GET_PROJECTS } from '../../queries/project';
+import ProjectCard from '../ProjectCard/ProjectCard';
+
+import '../../styles/components/_projectList.scss';
+
+const ProjectsList = () => {
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+
+  console.log(data);
+
+  if (loading) return <p>Loading..</p>;
+  if (error) return <p>Something went wrong..</p>;
+
+  return (
+    <>
+      <h3 className='projectList-header'>Projects</h3>
+      <section style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {data.projects.map((project: any) => {
+          return <ProjectCard project={project} />;
+        })}
+      </section>
+    </>
+  );
+};
+
+export default ProjectsList;
