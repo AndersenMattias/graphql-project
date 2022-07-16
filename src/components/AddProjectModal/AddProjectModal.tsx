8e3103263e799261edd43877088318844f43133a
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-07-16 14:12:01 +0200

    add modal for new Project with mutation and query

--- src/components/AddProjectModal/AddProjectModal.tsx
+++ src/components/AddProjectModal/AddProjectModal.tsx
@@ -1,14 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
 
 import '../../styles/components/_addProjectModal.scss';
 import Button from '../ui/Button';
+import { GET_CLIENTS } from '../../queries/client';
+import { ADD_PROJECT } from '../../mutations/project';
+import { GET_PROJECTS } from '../../queries/project';
 
-const AddProjectModal = () => {
+const AddProjectModal = (): JSX.Element => {
   const [isModalOpen, setModalIsOpen] = useState<boolean>(false);
 
-  const onToggleModal = () => {
-    setModalIsOpen(!isModalOpen);
-  };
+  // Form state
+  const [name, setName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [clientId, setClientId] = useState<string>('');
+  const [status, setStatus] = useState('Not_Started');
+
+  // Error handling
+  const [displayErr, setDisplayErr] = useState<boolean>(false);
+  const [errMessage, setErrMessage] = useState<string>('');
+
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  const [insert_projects_one] = useMutation(ADD_PROJECT, {
+    update(cache, { data }) {
+      const { projects }: any = cache.readQuery({
+        query: GET_PROJECTS,
+      });
+
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: {
+          projects: [data.insert_projects_one, ...projects],
+        },
+      });
+    },
+  });
+
+  useEffect(() => {
+    if (name.length > 0 || description.length > 0 || status.length > 0) {
+      setDisplayErr(false);
+      return;
+    }
+  }, [name, description, status]);
 
   useEffect(() => {
     //TODO: Fix event type
@@ -24,6 +58,41 @@ const AddProjectModal = () => {
     };
   }, []);
 
+  const onToggleModal = () => {
+    setModalIsOpen(!isModalOpen);
+  };
+
+  const onAddProject = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (name === '' || description === '' || status === '') {
+      setDisplayErr(true);
+      setErrMessage('Please fill in all fields.');
+      return;
+    }
+
+    insert_projects_one({
+      variables: {
+        name: name,
+        description: description,
+        clientId: clientId,
+        status: status,
+      },
+    });
+
+    setName('');
+    setDescription('');
+    setStatus('new');
+    setClientId('');
+
+    setTimeout(() => {
+      setModalIsOpen(false);
+    }, 1000);
+  };
+
+  if (loading) return null!;
+  if (error) return <p>Something went wrong.</p>;
+
   return (
     <>
       <button onClick={() => setModalIsOpen(!isModalOpen)}>New Project</button>
@@ -34,20 +103,57 @@ const AddProjectModal = () => {
             <h3 className='modal-title'>New Project</h3>
 
             <div className='modal-content'>
-              <form>
+              <form onSubmit={onAddProject}>
                 <label>Name</label>
-                <input type='text' />
-              </form>
-            </div>
+                <input
+                  type='text'
+                  id='name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <label>Description</label>
+                <textarea
+                  id='description'
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+                <label>Status</label>
+                <select
+                  id='status'
+                  value={status}
+                  onChange={(e) => setStatus(e.target.value)}
+                >
+                  <option value='Not_Started'>Not started</option>
+                  <option value='In_Progress'>In Progress</option>
+                  <option value='Completed'>Completed</option>
+                </select>
 
-            <div className='modal-btnContainer'>
-              <Button
-                type='button'
-                colour='btn--primary'
-                text='Close'
-                onClick={onToggleModal}
-              />
-              <Button type='button' colour='btn--primary' text='Submit' />
+                <div>
+                  <label>Client</label>
+                  <select
+                    id='clientId'
+                    value={clientId}
+                    onChange={(e) => setClientId(e.target.value)}
+                  >
+                    <option value=''>Select Client</option>
+                    {data.clients.map(({ id, name }: any) => (
+                      <option key={id} value={id}>
+                        {name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <div className='modal-btnContainer'>
+                  <Button
+                    type='button'
+                    colour='btn--primary'
+                    text='Close'
+                    onClick={onToggleModal}
+                  />
+                  <Button type='submit' colour='btn--primary' text='Submit' />
+                </div>
+              </form>
             </div>
           </div>
         </div>
